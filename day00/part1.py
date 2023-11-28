bc5c17edaabdf9627751ecc0479f6ee6d396c748
maxdemaio <maxdman321@gmail.com>
2023-11-27 22:13:36 -0500

    update

--- day00/part1.py
+++ day00/part1.py
@@ -1,13 +1,12 @@
 import os.path
-import pytest
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-def compute(s: str) -> int:
+def solve(s: str) -> int:
     lines = s.splitlines()
     for line in lines:
-        pass
+        print(line)
     # TODO: implement solution here!
     return 0
 
@@ -15,19 +14,18 @@ def compute(s: str) -> int:
 INPUT_S = '''\
 
 '''
-EXPECTED = 1
+EXPECTED = 0
 
 
 def test() -> None:
-    assert compute(INPUT_S) == EXPECTED
+    assert solve(INPUT_S) == EXPECTED
 
 
 def main() -> int:
     with open(INPUT_TXT) as f:
-        print(compute(f.read()))
-
+        print(solve(f.read()))
     return 0
 
 
 if __name__ == '__main__':
-    raise SystemExit(main())
+    main()
